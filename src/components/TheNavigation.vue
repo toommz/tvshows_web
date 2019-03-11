fdc9bb46e4857f96aeb746f3607950acd59d1859
Thomas Battiston <thomas.battiston@gmail.com>
2019-03-11 18:13:25 +0100

    Add TheNavigation component.

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item">TvShows</router-link>
+
+      <a
+        role="button"
+        v-bind:class="{ 'is-active': isActive }"
+        class="navbar-burger burger"
+        v-on:click="onBurgerClick"
+        aria-label="menu"
+        aria-expanded="false"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div v-bind:class="{ 'is-active': isActive }" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">Home</router-link>
+        <router-link to="/about" class="navbar-item">About</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "the-navigation",
+  data: function() {
+    return {
+      isActive: false
+    };
+  },
+  methods: {
+    onBurgerClick: function() {
+      this.isActive = !this.isActive;
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
+import Buefy from "buefy";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
+import "buefy/dist/buefy.css";
+import "@mdi/font/css/materialdesignicons.min.css";
+
+Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 new Vue({
